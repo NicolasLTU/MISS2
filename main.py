9b7462b64e6c245f679b75f39b7f0746ab81fdf7
Nicolas Martinez <57869789+NicolasLTU@users.noreply.github.com>
2024-04-08 19:56:43 +0200

    Update main.py
    Significant changes to the software.

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import signal
 import subprocess
 import time
 import serial
+from datetime import datetime
 
-import solar_zenith_calculator #Program used to check if it's daytime at KHO (Kjell Henriksen Observatory), returns a Boolean
+from solar_zenith_calculator import it_is_daytime #Program used to check if it's daytime at KHO (Kjell Henriksen Observatory), returns a Boolean
 from sunshield_controller import SunShield_CLOSE, SunShield_OPEN, init_serial #Control of the SunShield shutter: Close, Open, Settings for communication to the Serial Port 'COM3'
+
+#from fakezenith import it_is_daytime2 #Test the Atik responsiveness during switch from night to day time.
 
 
 #import AtikSDK
@@ -38,7 +41,7 @@ running = True  # To manage the while loop
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-
+    #start_time = datetime.now()
 try:
     ser = init_serial()
 except serial.SerialException as e:
@@ -47,16 +50,18 @@ except serial.SerialException as e:
 
 
     # Start keogram_maker and RGB_column_maker
-    keogram_process = subprocess.Popen(["python", "C:\\Users\\auroras\\.venvMISS2\\MISS2\\MISS2_Software\\keogram_maker.py"])
+    keogram_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\keogram_maker.py"])
     processes.append(keogram_process)
-    rgb_column_process = subprocess.Popen(["python", "C:\\Users\\auroras\\.venvMISS2\\MISS2\\MISS2_Software\\RGB_column_maker.py"])
+    rgb_column_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\RGB_column_maker.py"])
     processes.append(rgb_column_process)
+    average_png_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\average_PNG_maker.py"])
+    processes.append(average_png_process)
 
     image_capture_process = None  # Track the Atik_controller.py process
 
     try:
         while running:
-            if solar_zenith_calculator.is_it_daytime():  # If it's daytime, stop processes 
+            if it_is_daytime():  # If it's daytime, stop processes 
                 print ('Daytime: MISS2 is OFF')
                 if image_capture_process:
                     print("Terminating image capture process...")
@@ -71,9 +76,9 @@ except serial.SerialException as e:
                     if ser:
                         SunShield_OPEN(ser) #Open the SunShield
                     print("Starting Atik_controller.py...")
-                    image_capture_process = subprocess.Popen(["python", "C:\\Users\\auroras\\.venvMISS2\\MISS2\\MISS2_Software\\Atik_controller.py"])
-                    processes.append(image_capture_process)
-            time.sleep(10)  # Check every minute
+                    image_capture_process = subprocess.Popen(["python", "C:\\Users\\auroras\\.venvMISS2\\MISS2\\MISS2_Software\\capture_Atik.py"])
+                    processes.append(image_capture_process)          
+            time.sleep(60)  # Check every minute
     except KeyboardInterrupt:
         print("Interrupt received, cleaning up...")
     finally:
