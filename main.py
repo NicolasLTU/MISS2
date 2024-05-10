e8412f68ad553e2ac06cb550464d2866b00fd1db
Nicolas Martinez <57869789+NicolasLTU@users.noreply.github.com>
2024-05-11 00:18:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ import time
 import serial
 from datetime import datetime
 
-from solar_zenith_calculator import it_is_daytime #Program used to check if it's daytime at KHO (Kjell Henriksen Observatory), returns a Boolean
+from astronomical_twilight_calculator import it_is_nighttime #Program used to check if the Sun is below -10 degrees of elevation at KHO (Kjell Henriksen Observatory), returns a Boolean
 from sunshield_controller import SunShield_CLOSE, SunShield_OPEN, init_serial #Control of the SunShield shutter: Close, Open, Settings for communication to the Serial Port 'COM3'
 
-#from fakezenith import it_is_daytime2 #Test the Atik responsiveness during switch from night to day time.
-#import AtikSDK
+
+processes = []
+camera = None
+running = True  # To manage the while loop
+image_capture_process = None
+is_currently_night = None 
+
 
 def stop_processes(processes, timeout=5):
     for process in processes:
@@ -33,10 +38,6 @@ def signal_handler(sig, frame):
     print("Stopped the program.")
     exit(0)
 
-processes = []
-camera = None
-running = True  # To manage the while loop
-
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     #start_time = datetime.now()
@@ -48,35 +49,39 @@ except serial.SerialException as e:
 
 
     # Start keogram_maker and RGB_column_maker
-    keogram_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\keogram_maker.py"])
+    keogram_process = subprocess.Popen(["python", "C:/Users/auroras/.venvMISS2/MISS2/MISS2_Software/keogram_maker.py"])
     processes.append(keogram_process)
-    rgb_column_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\RGB_column_maker.py"])
+    rgb_column_process = subprocess.Popen(["python", "C:/Users/auroras/.venvMISS2/MISS2/MISS2_Software/RGB_column_maker.py"])
     processes.append(rgb_column_process)
-    average_png_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\average_PNG_maker.py"])
+    average_png_process = subprocess.Popen(["python", "C:/Users/auroras/.venvMISS2/MISS2/MISS2_Software/average_PNG_maker.py"])
     processes.append(average_png_process)
-    image_analyser_process = subprocess.Popen(["python", r"C:\Users\auroras\.venvMISS2\MISS2\MISS2_Software\image_analyser.py"])
+    image_analyser_process = subprocess.Popen(["python", "C:/Users/auroras/.venvMISS2/MISS2/MISS2_Software/image_analyser.py"])
     processes.append(image_analyser_process)
     image_capture_process = None  # Track the Atik_controller.py process
 
     try:
         while running:
-            if it_is_daytime():  # If it's daytime, stop processes 
-                print ('Daytime: MISS2 is OFF')
-                if image_capture_process:
-                    print("Terminating image capture process...")
-                    stop_processes([image_capture_process])
-                    processes.remove(image_capture_process)
-                    image_capture_process = None  # Reset the variable
+            if it_is_nighttime():
+                if is_currently_night is not True: # Check for transition day-night
+                    is_currently_night = True
+                    print ('Daytime: MISS2 is Operational')
                 if ser:
                     SunShield_CLOSE(ser) #Close the SunShield
-            else:  # Nighttime
-                print ('Nighttime: MISS2 is Operational')
-                if not image_capture_process:  # If Atik_controller.py isn't running
+                if not image_capture_process:
+                    image_capture_process = subprocess.Popen(["python", "path_to_capture_script.py"])
+                    processes.append(image_capture_process)
+
+            else:  # Daytime
+                if is_currently_night is not False: # Check for transition night-day
+                    is_currently_night = False
+                    print ("Daytime: MISS 2 is OFF")
+                    if image_capture_process:
+                        print("Stopping image capture...")
+                        stop_processes([image_capture_process])
+                        processes.remove(image_capture_process)
+                        image_capture_process = None
                     if ser:
-                        SunShield_OPEN(ser) #Open the SunShield
-                    print("Starting Atik_controller.py...")
-                    image_capture_process = subprocess.Popen(["python", "C:\\Users\\auroras\\.venvMISS2\\MISS2\\MISS2_Software\\capture_Atik.py"])
-                    processes.append(image_capture_process)          
+                        SunShield_CLOSE(ser) # Close the SunShield
             time.sleep(60)  # Check every minute
     except KeyboardInterrupt:
         print("Interrupt received, cleaning up...")
